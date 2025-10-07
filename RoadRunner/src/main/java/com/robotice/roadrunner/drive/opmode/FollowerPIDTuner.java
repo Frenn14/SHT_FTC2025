4910f45116166ae4a394de8cf7ab9511a1cdd8e7
Frenn <implan9102@gmail.com>
2025-10-07 21:21:35 +0900

    RoadRunner TeleOp Disabled

--- RoadRunner/src/main/java/com/robotice/roadrunner/drive/opmode/FollowerPIDTuner.java
+++ RoadRunner/src/main/java/com/robotice/roadrunner/drive/opmode/FollowerPIDTuner.java
@@ -3,6 +3,7 @@ package com.robotice.roadrunner.drive.opmode;
 import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import com.robotice.roadrunner.drive.SampleMecanumDrive;
@@ -23,6 +24,7 @@ import com.robotice.roadrunner.trajectorysequence.TrajectorySequence;
  */
 @Config
 @Autonomous(group = "drive")
+@Disabled
 public class FollowerPIDTuner extends LinearOpMode {
     public static double DISTANCE = 48; // in
 
--- RoadRunner/src/main/java/com/robotice/roadrunner/drive/opmode/TurnTest.java
+++ RoadRunner/src/main/java/com/robotice/roadrunner/drive/opmode/TurnTest.java
@@ -2,6 +2,7 @@ package com.robotice.roadrunner.drive.opmode;
 
 import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import com.robotice.roadrunner.drive.SampleMecanumDrive;
@@ -11,6 +12,7 @@ import com.robotice.roadrunner.drive.SampleMecanumDrive;
  */
 @Config
 @Autonomous(group = "drive")
+@Disabled
 public class TurnTest extends LinearOpMode {
     public static double ANGLE = 90; // deg
 
